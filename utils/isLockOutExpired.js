d04323c11f75ed0550276d5169d1034e41907530
rawnakabedalhade <144779978+rawnakabedalhade@users.noreply.github.com>
2024-04-03 21:45:05 +0300

    readme

--- utils/isLockOutExpired.js
+++ utils/isLockOutExpired.js
@@ -1,6 +1,6 @@
 const isLockoutExpired = (lastFailedLoginTimestamp) => {
   // Define the lockout duration in milliseconds (e.g., 24 hours)
-  const lockoutDurationMs = 24 * 60 * 60 * 1000; // 24 hours in milliseconds
+  const lockoutDurationMs = 4 * 60 * 60 * 1000; // 4 hours in milliseconds
   const currentTime = new Date().getTime();
   const lastFailedLoginTime = new Date(lastFailedLoginTimestamp).getTime();
 
